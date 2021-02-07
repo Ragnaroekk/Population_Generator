3416d394df2db7514597700afc9e2258a76a3ca2
Ragnaroekk <rayzor007@gmail.com>
2021-02-06 16:59:47 -0800

    testing tkinter

--- population-generator.py
+++ population-generator.py
@@ -7,8 +7,50 @@ import tkinter as tk
 
 
 window = tk.Tk()
+window.title("Test Title")
 
 greeting = tk.Label(text="Hello, Tkinter")
 
 greeting.pack()
 
+label = tk.Label(
+    text="Hello, Tkinter",
+    fg="white",
+    bg="black",
+    width=10,
+    height=10
+)
+
+button = tk.Button(
+    text="Display Results!",
+    width=10,
+    height=5,
+    bg="green",
+    fg="white",
+)
+
+button.pack()
+
+frame_a = tk.Frame()
+frame_b = tk.Frame()
+
+label_a = tk.Label(master=frame_a, text="I'm in Frame A")
+label_a.pack()
+
+label_b = tk.Label(master=frame_b, text="I'm in Frame B")
+label_b.pack()
+
+frame_a.pack()
+frame_b.pack()
+
+state = tk.Label(text="State:")
+entry = tk.Entry(fg="yellow", bg="blue", width=50)
+
+state.pack()
+entry.pack()
+
+test = entry.get()
+print(test)
+entry.delete(0, tk.END)
+
+window.mainloop()
