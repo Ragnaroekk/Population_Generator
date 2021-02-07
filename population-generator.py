165e42b62abd39eda1626735249033389e1c8b1e
Ragnaroekk <rayzor007@gmail.com>
2021-02-06 16:11:11 -0800

    testing tkinter

---
+++ population-generator.py
@@ -0,0 +1,14 @@
+# Author: Ray Franklin
+# Population Generator CS361
+# Built with research from https://realpython.com/python-gui-tkinter/
+
+
+import tkinter as tk
+
+
+window = tk.Tk()
+
+greeting = tk.Label(text="Hello, Tkinter")
+
+greeting.pack()
+
